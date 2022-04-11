dc01ad6ebdc6d4870fe074855c3047a80de83a75
shun1121 <hikitanikun.ware80000@gmail.com>
2022-04-11 15:46:55 +0900

    💄 split style files

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,12 @@
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 import { Links } from "./Links";
 import { HeadLine } from "./HeadLine";
 
 export function Main(props) {
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <HeadLine page={props.title}>
-        <code className={styles.code}>pages/{props.title}.js</code>
+        <code className={classes.code}>pages/{props.title}.js</code>
       </HeadLine>
       <Links />
     </main>
