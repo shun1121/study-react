06779ead165d987607353e9147fbd8a19af15407
shun1121 <hikitanikun.ware80000@gmail.com>
2022-04-13 11:17:06 +0900

    ⚡️ second argument of useCallback and useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,21 +4,22 @@ import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 import { useEffect, useState } from "react";
+import { useCallback } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1)
-  const handleClick = (e) => {
-    setCount(count => count + 1)
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount(count => count + 1)
+    }
+  }, [count]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
-
-  console.log(count)
+  }, [count]);
 
   return (
     <div className={styles.container}>
@@ -29,7 +30,7 @@ export default function Home() {
       <h1>{count}</h1>
       <button onClick={handleClick}>Button</button>
       <Main title="index" />
-      <countter />
+      <Footer />
     </div>
   );
 }
