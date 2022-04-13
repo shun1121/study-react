4f068e232d856533477e8cd4fbd9246da5a49940
shun1121 <hikitanikun.ware80000@gmail.com>
2022-04-13 18:48:46 +0900

    ♻️ added prev when usig setState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,13 +13,13 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount(count => count + 1)
+      setCount(prevCount => prevCount + 1)
     }
   }, [count]);
 
-  const handleDisplay = () => {
-    setIsShow((isShow) => !isShow)
-  }
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow)
+  }, [])
 
   const handleChange = useCallback((e) => {
     console.log(e.target.value);
